49248338ff95727a3f45b6b41e003d702b42ae48
Ernest Keita <keitaernest@gmail.com>
2024-06-02 14:21:44 +0200

    inject data source into app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private dataSource: DataSource;
+  constructor(
+    @Inject('DATA_SOURCE')
+    dataSource: DataSource,
+  ) {
+    this.dataSource = dataSource;
   }
+
+  // async extractEntities(searchTerm: string) {
+
+  // }
 }
