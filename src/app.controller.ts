03b98c68db502e9f615b212de4cd3172ca9facc2
Ernest Keita <keitaernest@gmail.com>
2024-06-02 16:33:04 +0200

    Yay!! 70 %

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ExtractEntitiesQueryDto } from './dto/extract-entities-query.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return 'hello world';
+  @Get('/search')
+  async search(
+    @Query() requestQueryParams: ExtractEntitiesQueryDto,
+  ): Promise<any> {
+    const entities = await this.appService.extractEntities(
+      requestQueryParams.searchTerm,
+    );
+    console.log('🚀 ~ AppController ~ search ~ entities:', entities);
+    return entities;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
+import { City } from './models/city.entity';
+import { Brand } from './models/brand.entity';
+import { DishType } from './models/dish-type.entity';
+import { Diet } from './models/diet.entity';
 
 @Injectable()
 export class AppService {
@@ -11,7 +15,29 @@ export class AppService {
     this.dataSource = dataSource;
   }
 
-  // async extractEntities(searchTerm: string) {
+  async extractEntities(searchTerm: string) {
+    // Split the searchTerm into words
+    const terms = searchTerm.split(/\s+/);
+    console.log('🚀 ~ AppService ~ extractEntities ~ terms:', terms);
+    const placeholders = terms.map((_, index) => _).join(', ');
 
-  // }
+    console.log(
+      '🚀 ~ AppService ~ extractEntities ~ placeholders:',
+      placeholders,
+    );
+    // Create a single query using UNION ALL to check all entities
+    const query = `
+    SELECT id,name,  'city' as 'type'  FROM city WHERE name IN (${terms.map((value) => `'${value}'`)})
+    UNION
+    SELECT id,name, 'brand' as 'type' FROM brand WHERE name IN (${terms.map((value) => `'${value}'`)})
+    UNION
+    SELECT id,name, 'dishType' as 'type' FROM dish_type WHERE name IN (${terms.map((value) => `'${value}'`)})
+    UNION
+    SELECT id,name, 'diet' as 'type' FROM diet WHERE name IN (${terms.map((value) => `'${value}'`)})
+  `;
+
+    const result = await this.dataSource.query(query, terms);
+
+    return result;
+  }
 }
