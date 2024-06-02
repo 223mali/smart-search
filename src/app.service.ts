cddfd6cdf815db1c45432e3e0031925684461d69
Ernest Keita <keitaernest@gmail.com>
2024-06-02 20:08:21 +0200

    exclude all words shorter than 3 characters

--- src/app.service.ts
+++ src/app.service.ts
@@ -29,19 +29,20 @@ export class AppService {
   async extractEntities(searchTerm: string): Promise<EntityType[]> {
     // Split the searchTerm into words
     const terms = searchTerm.split(/\s+/);
+    const filteredTerms = terms.filter((term) => term.length > 2);
 
     // Create a single query using UNION ALL to check all entities
     const query = `
-    SELECT id,name,  'city' as 'type'  FROM city WHERE ${this.parseSearchTerm(terms)}
+    SELECT id,name,  'city' as 'type'  FROM city WHERE ${this.parseSearchTerm(filteredTerms)}
     UNION ALL
-    SELECT id,name, 'brand' as 'type' FROM brand WHERE ${this.parseSearchTerm(terms)}
+    SELECT id,name, 'brand' as 'type' FROM brand WHERE ${this.parseSearchTerm(filteredTerms)}
     UNION ALL
-    SELECT id,name, 'dishType' as 'type' FROM dish_type WHERE ${this.parseSearchTerm(terms)}
+    SELECT id,name, 'dishType' as 'type' FROM dish_type WHERE ${this.parseSearchTerm(filteredTerms)}
     UNION ALL
-    SELECT id,name, 'diet' as 'type' FROM diet WHERE ${this.parseSearchTerm(terms)}
+    SELECT id,name, 'diet' as 'type' FROM diet WHERE ${this.parseSearchTerm(filteredTerms)}
   `;
 
-    const result = await this.dataSource.query(query, terms);
+    const result = await this.dataSource.query(query, filteredTerms);
 
     return result as EntityType[];
   }
